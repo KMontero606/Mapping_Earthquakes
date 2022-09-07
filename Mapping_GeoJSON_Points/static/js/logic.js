ebd842acb336a75b6c5f8a2aa70cd2de3dcfda5c
KMontero606 <kayemontero@gmail.com>
2022-09-07 12:43:42 -0700

    Added mapping multiple maps and multiple geojson points

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -37,32 +37,21 @@ let sanFranAirport =
 // Skill Drill
 // Edit your logic.js to create a popup marker for San Francisco Airport on a night preview navigation map.
 // When you click on the popup, it will display the city, state, and the name of the airport.
-//L.geoJSON(sanFranAirport, {
-    // We turn each feature into a marker on the map.
-    //pointToLayer: function(feature, latlng) {
-        //console.log(feature);
-        //return L.marker(latlng)
-        //.bindPopup("<h2>" + feature.properties.name + " " + feature.properties.city + ", " + feature.properties.country + "</h2>");
-    //}
-//}).addTo(map);
-
-// Grabbing our GeoJSON data.
 L.geoJSON(sanFranAirport, {
-    onEachFeature: function(feature, layer) {
-        console.log(layer);
-        layer.bindPopup("<h2>" + "Airport code: " + feature.properties.faa + " " + "Airport name: " + feature.properties.name + "</h2>");
+    // We turn each feature into a marker on the map.
+    pointToLayer: function(feature, latlng) {
+        console.log(feature);
+        return L.marker(latlng)
+        .bindPopup("<h2>" + feature.properties.name + "</h2>" + "<h3>" + feature.properties.city + ", " + feature.properties.country + "</h3>");
     }
 }).addTo(map);
 
-// Skill Drill
-// Edit your logic.js to create a popup marker for the San Francisco Airport on the outdoor map.
-// When you click on the popup, it will display the airport code and name of the airport.
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/outdoors-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/navigation-night-v1/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
 });
 
-// Then we add our 'graymap' tile layer to the map.
+// Then we add our 'navigation night' tile layer to the map.
 streets.addTo(map);
